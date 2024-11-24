ea910903c11baf97471487687c9d567d26a129e5
Nico Govindsamy <ngovindsamy18@gmail.com>
2024-11-24 22:44:24 +0200

    handled all moves as events

--- gui.py
+++ gui.py
@@ -6,20 +6,24 @@ from enum import Enum
 import quarto_agents
 
 BLANK_TILE = 16
+AGENT_DELAY_MS = 1500
+
 
 class MoveType(Enum):
     PLACE_PIECE = 1
     PICK_PIECE = 2
 
+
 class QuartoGUI(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("Quarto Game")
-        self.player1IsHuman = False
-        self.player2IsHuman = True
-        self._game = QuartoGame(
-            quarto_agents.HumanPlayer(), quarto_agents.HumanPlayer(), gui_mode=True, bin_mode=False
-        )
+        self.player1IsHuman = True
+        self.player2IsHuman = False
+        self.player1 = quarto_agents.RandomAgent()
+        self.player2 = quarto_agents.NegamaxAgent(depth=2, searchWindow=64)
+        self._game = QuartoGame(self.player1, self.player2, gui_mode=True, bin_mode=False)
+        self.moveCounter = 0
         self.isPlayerOneTurn = True
         self.moveType = MoveType.PICK_PIECE
 
@@ -54,7 +58,7 @@ class QuartoGUI(tk.Tk):
             font=font.Font(size=28, weight="bold"),
         )
         self.display2 = tk.Label(
-            master=display_frame, text="Player 1's turn", font=font.Font(size=18, weight="bold")
+            master=display_frame, text="PRESS START", font=font.Font(size=18, weight="bold")
         )
         self.currentButton = tk.Button(
             master=display_frame,
@@ -80,18 +84,66 @@ class QuartoGUI(tk.Tk):
         self.startButton.pack(side=tk.RIGHT)
         self.startButton.bind("<ButtonPress-1>", self.play)
 
-    def _placePiece(self, row, col):
+    def _checkIfGameOver(self):
+        if qutil.isGameOver(self._game.board):
+            self._toggleGridFreeze()
+            if self.isPlayerOneTurn:
+                self.display["text"] = "Player 1 Won!"
+                self.display2["text"] = ""
+            else:
+                self.display["text"] = "Player 2 Won!"
+                self.display2["text"] = ""
+            return True
+        elif self.moveCounter == 14:
+            self._toggleGridFreeze()
+            self.display["text"] = "DRAW!"
+            self.display2["text"] = ""
+            return True
+        return False
+
+    def _updateState(self):
+        self.moveCounter += 1
+        self.isPlayerOneTurn = not self.isPlayerOneTurn
+        self.moveType = MoveType.PLACE_PIECE
+        self.display["text"] = f"Player {1 if self.isPlayerOneTurn else 2}'s Turn"
+
+        isHumanTurn = (self.isPlayerOneTurn and self.player1IsHuman) or (
+            not self.isPlayerOneTurn and self.player2IsHuman
+        )
+        if isHumanTurn:
+            self.display2["text"] = "Place your piece"
+            self._toggleGridFreeze(toggleFreezeOn=False)
+        else:
+            self._toggleGridFreeze()
+            self.display2["text"] = "Agent is placing current piece"
+
+        return isHumanTurn
+
+    def _handleMoveEnd(self):
+        if self._checkIfGameOver():
+            return
+        isHumanTurn = self._updateState()
+        if not isHumanTurn:
+            self.after(AGENT_DELAY_MS, self.makeAgentMove)
+
+    def _placePieceEvent(self, position):
         if self.moveType == MoveType.PICK_PIECE:
             self.display2["text"] = "Please pick a piece"
+            return
 
-        self.updateCell
-        pass
+        self.placePiece(position)
+        if self._checkIfGameOver():
+            return
+        self.moveType = MoveType.PICK_PIECE
+        self.display2["text"] = "Pick your opponent's next piece"
 
-    def _pickPiece(self, row, col):
+    def _pickPieceEvent(self, nextPiece):
         if self.moveType == MoveType.PLACE_PIECE:
             self.display2["text"] = "Please place your current piece first"
+            return
 
-        pass
+        self.pickNextPiece(nextPiece)
+        self._handleMoveEnd()
 
     def _createBoardGrid(self):
         self._cells = {}
@@ -100,17 +152,18 @@ class QuartoGUI(tk.Tk):
         grid_frame.pack(padx=50, pady=50, side=tk.LEFT)
         for row in range(4):
             for col in range(4):
+                linearCoords = 4 * row + col
                 button = tk.Button(
                     master=grid_frame,
                     fg="black",
                     width=100,
                     height=100,
-                    text=4 * row + col,
+                    text=linearCoords,
                     font=("Helvetica 12 bold"),
                     image=self._photos[BLANK_TILE],
                     compound="center",
                     bd=0,
-                    command=lambda row=row, col=col : self._placePiece(row, col)
+                    command=lambda position=linearCoords: self._placePieceEvent(position),
                 )
                 self._cells[(row, col)] = button
                 button.grid(row=row, column=col, padx=3, pady=3, sticky="nsew")
@@ -125,15 +178,16 @@ class QuartoGUI(tk.Tk):
             self.rowconfigure(row, weight=1, minsize=50)
             self.columnconfigure(row, weight=1, minsize=75)
             for col in range(4):
+                linearCoords = 4 * row + col
                 button = tk.Button(
                     master=grid_frame,
-                    text=str(4 * row + col),
+                    text=str(linearCoords),
                     font=("Helvetica 15 bold"),
                     image=self._photos[4 * row + col],
                     compound=tk.RIGHT,
                     relief=tk.FLAT,
                     bd=0,
-                    command=lambda row=row, col=col : self._pickPiece(row, col)
+                    command=lambda position=linearCoords: self._pickPieceEvent(position),
                 )
                 self._pieces[(row, col)] = button
                 button.grid(row=row, column=col, padx=5, pady=5)
@@ -141,166 +195,94 @@ class QuartoGUI(tk.Tk):
     def _resetGrids(self):
         for row in range(4):
             for col in range(4):
-                self._cells[(row, col)].configure(image=self._photos[BLANK_TILE], text=4 * row + col)
+                self._cells[(row, col)].configure(
+                    image=self._photos[BLANK_TILE], text=4 * row + col, state=tk.ACTIVE
+                )
                 self._pieces[(row, col)].configure(state=tk.ACTIVE)
         self.currentButton.configure(image=self._photos[BLANK_TILE], text="current")
 
-    def _resetBoard(self, event):
+    def _resetState(self, event):
         self._game.resetGame()
+        self.moveCounter = 0
+        self.isPlayerOneTurn = True
+        self.moveType = MoveType.PICK_PIECE
         self._takenCells.clear()
         self._takenPieces.clear()
         self._resetGrids()
         self._update_display(msg="Ready?")
         self.play(event)
 
-    def isValidMove(self, position, nextPiece):
-        if position not in range(16):
-            self.display2["text"] = "This position does not exist"
-            return False
-        if nextPiece not in range(16):
-            self.display2["text"] = "This piece does not exist"
-            return False
-        if position not in self._game.availablePositions:
-            self.display2["text"] = "This cell is unavailable"
-            return False
-        if nextPiece not in self._game.availablePieces:
-            self.display2["text"] = "This piece is not available"
-            return False
-        return True
-    
-
-    def makeMove(self, position, nextPiece):
-        # validation checks
-        if not self.isValidMove(position, nextPiece):
-            return False
-
-        # add to move history
-        self._game.moveHistory.append((position, nextPiece))
-
-        # place piece on board and update game state
-        self.updateCell(position)
+    def placePiece(self, position):
+        self._updateCell(position)
         row, col = qutil.get2dCoords(position)
         self._game.board[row][col] = self._game.currentPiece
         self._game.availablePositions.remove(position)
 
-        # set next player's piece
-        self.updateCurrent(nextPiece)
+    def pickNextPiece(self, nextPiece):
+        self._updateCurrent(nextPiece)
         self._game.currentPiece = nextPiece
         self._game.availablePieces.remove(self._game.currentPiece)
-
         if self._game.gui_mode:
             print("Move successful")
-        return True
+            self._game.showGameState()
 
     def makeAgentFirstMove(self):
         firstMove = self._game.pickRandomAvailablePiece()
         self._game.makeFirstMove(firstMove)
-        self.updateCurrent(firstMove)
+        self._updateCurrent(firstMove)
 
         if self._game.gui_mode:
             self._game.showGameState()
-        self.toggleGridFreeze(toggleFreezeOn=False)
+        self._handleMoveEnd()
 
-        self.isPlayerOneTurn = False
-        self.moveType = MoveType.PLACE_PIECE
-        self.display["text"] = "Player 2's Turn"
-        self.display2["text"] = "Place your piece"
-    
+    def makeAgentMove(self):
+        position, nextPiece = None, None
+        if self.isPlayerOneTurn:
+            position, nextPiece = self.player1.makeMove(
+                self._game.getGameState(), gui_mode=self._game.gui_mode
+            )
+        else:
+            position, nextPiece = self.player2.makeMove(
+                self._game.getGameState(), gui_mode=self._game.gui_mode
+            )
+
+        self.placePiece(position)
+        if self._checkIfGameOver():
+            return
+
+        self.pickNextPiece(nextPiece)
+        if self._game.gui_mode:
+            self._game.showGameState()
+        self._handleMoveEnd()
+
     def play(self, event):
         self.display["text"] = "Player 1's turn"
-        
-        if not self.player1IsHuman:
-            self.toggleGridFreeze()
-            self.display2["text"] = "Agent is picking the first piece"
-            self.after(2000, self.makeAgentFirstMove)
 
-        self.startButton.bind("<ButtonPress-1>", self._resetBoard)
+        if self.player1IsHuman:
+            self.display2["text"] = "Pick your opponent's first piece"
+        else:
+            self._toggleGridFreeze()
+            self.display2["text"] = "Agent is picking the first piece"
+            self.after(AGENT_DELAY_MS, self.makeAgentFirstMove)
+
+        self.startButton.bind("<ButtonPress-1>", self._resetState)
         self.startButton.configure(text="Reset")
 
-        # subsequent moves
-        # for i in range(len(self._game.availablePositions) - 1):
-        #     if self._game.gui_mode:
-        #         self._game._showPlayerName(isPlayerOneTurn)
-
-        #     # player 1
-        #     if isPlayerOneTurn:
-        #         self.display["text"] = "Player 1's turn"
-        #         self.display2["text"] = "Agent is playing..."
-
-        #         for i in range(3):
-        #             position, nextPiece = self._game.player1.makeMove(
-        #                 self._game.getGameState(), gui_mode=self._game.gui_mode
-        #             )
-        #             validMove = self.makeMove(position, nextPiece)
-        #             if validMove:
-        #                 break
-        #             elif i == 2:
-        #                 print("Three invalid moves made - game ended")
-        #                 return
-        #     # player 2
-        #     else:
-        #         self.display["text"] = "Player 2's turn"
-        #         self.display2["text"] = "Agent is playing..."
-
-        #         for i in range(3):
-        #             position, nextPiece = self._game.player2.makeMove(
-        #                 self._game.getGameState(), gui_mode=self._game.gui_mode
-        #             )
-        #             validMove = self.makeMove(position, nextPiece)
-        #             if validMove:
-        #                 break
-        #             elif i == 2:
-        #                 print("Three invalid moves made - game ended")
-        #                 return
-
-        #     if self._game.gui_mode:
-        #         self._game.showGameState()
-
-        #     if qutil.isGameOver(self._game.board):
-        #         if isPlayerOneTurn:
-        #             self.display["text"] = "Player 1 Won!"
-        #             self.display2["text"] = ""
-        #         else:
-        #             self.display["text"] = "Player 2 Won!"
-        #             self.display2["text"] = ""
-        #         return
-        #     isPlayerOneTurn = not isPlayerOneTurn
-
-        # # Place last piece and set nextPiece to nothing
-        # if self._game.gui_mode:
-        #     self._game._showPlayerName(isPlayerOneTurn)
-        # self._game.makeLastMove()
-
-        # if self._game.gui_mode:
-        #     self._game.showGameState()
-
-        # if qutil.isGameOver(self._game.board):
-        #     if isPlayerOneTurn:
-        #         self.display["text"] = "Player 1 Won!"
-        #         self.display2["text"] = ""
-        #     else:
-        #         self.display["text"] = "Player 2 Won!"
-        #         self.display2["text"] = ""
-        #     return
-        # else:
-        #     self.display["text"] = "DRAW!"
-        #     self.display2["text"] = ""
-
-    def updateCurrent(self, piece):
+    def _updateCurrent(self, piece):
         target = self.currentButton
         target.configure(image=self._photos[piece], bg="#876c3e", text="")
         piece = self._pieces[qutil.get2dCoords(piece)]
         self._takenPieces.add(piece)
         piece.configure(state=tk.DISABLED)
 
-    def updateCell(self, position):
+    def _updateCell(self, position):
         piece = self._game.currentPiece
         target = self._cells[qutil.get2dCoords(position)]
         target.configure(image=self._photos[piece], bg="#876c3e", text="")
         self._takenCells.add(target)
         self.currentButton.configure(image=self._photos[BLANK_TILE], text="current")
 
-    def toggleGridFreeze(self, toggleFreezeOn=True):
+    def _toggleGridFreeze(self, toggleFreezeOn=True):
         if toggleFreezeOn:
             for row in range(4):
                 for col in range(4):
@@ -317,6 +299,7 @@ class QuartoGUI(tk.Tk):
     def _update_display(self, msg, color="black"):
         self.display["text"] = msg
         self.display["fg"] = color
+
 
 def main():
     gui = QuartoGUI()
