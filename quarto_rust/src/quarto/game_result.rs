7c269f0cd9e4e720a69ebbc2bf0ca8611de6df9e
Nico Govindsamy <ngovindsamy18@gmail.com>
2025-04-18 14:43:12 +0200

    added formatting for game result

--- quarto_rust/src/quarto/game_result.rs
+++ quarto_rust/src/quarto/game_result.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 pub enum GameResult {
     PlayerOneWon,
     PlayerTwoWon,
@@ -5,3 +7,16 @@ pub enum GameResult {
     PlayerOneInvalid,
     PlayerTwoInvalid,
 }
+
+impl fmt::Display for GameResult {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+       match self {
+           GameResult::PlayerOneWon => write!(f, "1"),
+           GameResult::PlayerTwoWon => write!(f, "2"),
+           GameResult::Draw => write!(f, "0"),
+           GameResult::PlayerOneInvalid => write!(f, "-1"),
+           GameResult::PlayerTwoInvalid => write!(f, "-2"),
+       }
+    }
+}
+
