26fd15cf56535914528064b18a10cb542882e74d
Nico Govindsamy <ngovindsamy18@gmail.com>
2025-05-01 11:48:37 +0200

    chromosome mutation

--- quarto_rust/src/quarto_agent/genetic_agent/chromosome.rs
+++ quarto_rust/src/quarto_agent/genetic_agent/chromosome.rs
@@ -1,6 +1,8 @@
-use crate::quarto::{quarto_game_state::QuartoGameState, QuartoMove};
+use rand::seq::{IndexedMutRandom, IteratorRandom};
 
-#[derive(Eq, Hash, PartialEq, Clone)]
+use crate::quarto::{QuartoMove, quarto_game_state::QuartoGameState};
+
+#[derive(Eq, Hash, PartialEq, Clone, Debug)]
 pub struct Chromosome {
     movepath: Vec<QuartoMove>,
 }
@@ -15,10 +17,29 @@ impl Chromosome {
     }
 
     pub fn crossover(&self, other: &Chromosome) -> Chromosome {
-        Chromosome { movepath: Vec::new() }
+        Chromosome {
+            movepath: Vec::new(),
+        }
     }
 
-    pub fn mutation(&mut self, state: &QuartoGameState) {}
+    pub fn mutation(&self, state: &QuartoGameState) -> Self {
+        let mut mutated_chromosome = self.clone();
+
+        let mut rng = rand::rng();
+        let quarto_move = mutated_chromosome.movepath.choose_mut(&mut rng).unwrap();
+        //mutate position
+        if rand::random::<f64>() < 0.8 {
+            let random_position = state.available_positions.iter().choose(&mut rng);
+            quarto_move.0 = random_position.unwrap().clone();
+        }
+        //mutate next piece
+        else {
+            let random_piece = state.available_pieces.iter().choose(&mut rng);
+            quarto_move.1 = random_piece.unwrap().clone();
+        }
+
+        mutated_chromosome
+    }
 
     pub fn is_valid(&self, state: &QuartoGameState) -> bool {
         true
