71b17e422f808b1164c16bbca1ce995bb53a3e31
Nico Govindsamy <ngovindsamy18@gmail.com>
2025-05-01 18:19:17 +0200

    chromosome validation

--- quarto_rust/src/quarto_agent/genetic_agent/chromosome.rs
+++ quarto_rust/src/quarto_agent/genetic_agent/chromosome.rs
@@ -1,4 +1,8 @@
-use rand::{seq::{IndexedMutRandom, IteratorRandom}, Rng};
+use rand::{
+    Rng,
+    seq::{IndexedMutRandom, IteratorRandom},
+};
+use std::collections::HashSet;
 
 use crate::quarto::{QuartoMove, quarto_game_state::QuartoGameState};
 
@@ -16,13 +20,13 @@ impl Chromosome {
         &self.movepath
     }
 
-    pub fn crossover(&self, other: &Chromosome) -> Self {
+    pub fn crossover(&self, other: &Chromosome) -> Option<Self> {
         let mut num_moves = self.movepath.len();
         if other.movepath.len() < num_moves {
             num_moves = other.movepath.len();
         }
         if num_moves < 2 {
-            return self.clone();
+            return None;
         }
 
         let point = rand::rng().random_range(1..num_moves);
@@ -30,10 +34,7 @@ impl Chromosome {
         new_path.extend_from_slice(&other.movepath[point..]);
 
         println!("cross at {point}  {:?}", new_path);
-
-        Chromosome {
-            movepath: new_path,
-        }
+        Some(Chromosome::new(new_path))
     }
 
     pub fn mutation(&self, state: &QuartoGameState) -> Self {
@@ -56,7 +57,33 @@ impl Chromosome {
     }
 
     pub fn is_valid(&self, state: &QuartoGameState) -> bool {
-        true
+        //invalid if moves repeat
+        let positions: HashSet<u8> = self.movepath.iter().map(|q| q.0).collect();
+        if positions.len() < self.movepath.len() {
+            return false;
+        }
+        let pieces: HashSet<u8> = self.movepath.iter().map(|q| q.1).collect();
+        if pieces.len() < self.movepath.len() {
+            return false;
+        }
+
+        //invalid if moves are not available in state
+        let pos_intersect: HashSet<u8> = positions
+            .intersection(&state.available_positions)
+            .cloned()
+            .collect();
+        if pos_intersect.len() < positions.len() {
+            return false;
+        }
+        let piece_intersect: HashSet<u8> = pieces
+            .intersection(&state.available_pieces)
+            .cloned()
+            .collect();
+        if piece_intersect.len() < pieces.len() {
+            return false;
+        }
+
+        return true;
     }
 }
 
